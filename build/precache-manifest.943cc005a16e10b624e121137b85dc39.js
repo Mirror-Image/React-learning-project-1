5c81bf99c051f2a23c2076681903936adf576132
Maksim Borovik <borovik.maksim@gmail.com>
2020-03-19 11:18:29 +0200

    created a build

---
+++ build/precache-manifest.943cc005a16e10b624e121137b85dc39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250aa5d55a912f677f3322bf5e25e2c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcc97ee29469f4d3b4a1",
+    "url": "/static/css/main.cd111c1e.chunk.css"
+  },
+  {
+    "revision": "508cb376b9c69694c24c",
+    "url": "/static/js/2.65d1f3ef.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.65d1f3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc97ee29469f4d3b4a1",
+    "url": "/static/js/main.53aa143c.chunk.js"
+  },
+  {
+    "revision": "54a23e02fb541e51bb8d",
+    "url": "/static/js/runtime-main.1fb5186a.js"
+  }
+]);
